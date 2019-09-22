b67039ec3ecec561036a1d8d720116d5d64b199c
ghpranav <bedrepranav@gmail.com>
2019-09-23 01:00:07 +0530

    initial DisplayBooks page

--- src/App.js
+++ src/App.js
@@ -1,89 +1,49 @@
 import React from 'react';
 import './App.css';
-import update from 'immutability-helper';
-
-class Item extends React.Component {
-  render() {
-    return (
-      <div className="Item">
-        {this.props.item.title}
-      </div>
-    );
-  }
-}
+import axios from 'axios';
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     items: []
-  //   };
-  // }
-  state = {
-    items: []
+  constructor() {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+
+  deleteBook = index => {
+    var array = [...this.state.books];
+    array.splice(index, 1);
+    this.setState({ books: array });
   }
 
   componentDidMount() {
-    fetch('https://dscjssstu.herokuapp.com/getbooks')
-      .then(res => res.json())
-      .then((data) => {
-        this.setState({ items: data })
-        console.log(this.state.items)
+    axios.get('https://dscjssstu.herokuapp.com/getbooks')
+      .then(res => res.data).then((data) => {
+        this.setState({ books: data });
       })
-      .catch(console.log)
   }
 
-  deleteBook = (index) => {
-    this.setState(state => {
-      const list = [update(this.state.items[index], { isVisible: { $set: '0' } })]
-      return list
-    })
-    // this.setState({
-    //   items: update(this.state.items[index], { isVisible: { $set: '0' } })
-    // })
-    // this.setState({ item.isVisible: false });
-  }
   render() {
     return (
       <div className="App">
         <div class="container">
           <div class="jumbotron">
-            <div class="text-center text-capitalize">
+            <div class="text-center">
               <h1>Books</h1>
             </div>
 
-            {/* <div>
-              {this.state.items.map((item, index) => (
-                (item.isVisible === 1) ? (
-                  <div>
-                    <Item key={index} item={item} />
-                    <button
-                      type="button"
-                      onClick={() => this.deleteBook(index)}
-                    >Delete</button>
-                  </div>) : null)
-              )}
-            </div> */}
-
-            {this.state.items.map((item) => (
+            {this.state.books.map((book, index) => (
               <div className="card">
                 <div className="card-body">
-                  <h5 className="card-title">{item.title}</h5>
+                  <h5 className="card-title">{book.title}</h5>
                   <h6 className="card-subtitle mb-2 text-muted">
-                    {item.completed &&
-                      <span>
-                        Completed
-                </span>
-                    }
-                    {!item.completed &&
-                      <span>
-                        Pending
-                </span>
-                    }
+                    {book.description}
                   </h6>
+                  <button onClick={() => this.deleteBook(index)} class="btn btn-primary stretched-link">Delete</button>
                 </div>
               </div>
             ))}
+
           </div>
         </div>
       </div>
