2e7eb2e798a8beef0b446859e28dab3c41b6b071
ghpranav <bedrepranav@gmail.com>
2019-09-23 15:26:44 +0530

    Add react-flippy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
 class App extends React.Component {
   constructor() {
@@ -30,17 +31,30 @@ class App extends React.Component {
 
 
   render() {
-    let displayBook;
-    displayBook = (
-      this.state.books.map((book, index) => (
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">{book.title}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">
-              {book.description}
-            </h6>
-            <button onClick={() => this.deleteBook(index)} class="btn btn-primary stretched-link">Delete</button>
-          </div>
+
+    let displayBook = (
+      this.state.books.map((book) => (
+        <div>
+          <Flippy
+            flipOnHover={false}
+            flipOnClick={true}
+            flipDirection="vertical"
+            ref={(r) => this.flippy = r}
+          >
+            <FrontSide
+              style={{
+                backgroundColor: '#ffffff',
+              }}>
+              <h5 className="card-title">{book.title}</h5>
+            </FrontSide>
+            <BackSide>
+              <h5 className="card-title">{book.title} - ${book.price}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                {book.description}
+              </h6>
+            </BackSide>
+          </Flippy>
+          <br />
         </div>
       ))
     )
@@ -52,9 +66,7 @@ class App extends React.Component {
             <div class="text-center">
               <h1>Books</h1>
             </div>
-
             {displayBook}
-
           </div>
         </div>
       </div>
